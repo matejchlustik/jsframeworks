51b3616bcbbbeb6242fd6bfd3e842ff8bd85091c
Matej Chlustik <matejchlustik221@gmail.com>
2023-04-07 14:05:14 +0200

    final

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,69 @@
 <template>
-    <Page>
-        <ActionBar>
-            <Label text="Home"/>
-        </ActionBar>
+  <Page>
+    <ActionBar>
+      <Label text="Home" />
+    </ActionBar>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
-    </Page>
+    <FlexboxLayout v-if="location" flexDirection="column" alignItems="center" justifyContent="center">
+      <Label :text="'latitude ' + location.latitude" />
+      <Label :text="'longitude ' + location.longitude" />
+      <Label :text="'horizontal accuracy ' + location.horizontalAccuracy" />
+      <Label :text="'vertical accuracy ' + location.verticalAccuracy" />
+      <Button @tap="getLocation" text="update" />
+      <Label v-if="msg" :text="msg" />
+    </FlexboxLayout>
+    <FlexboxLayout v-else>
+      <Label text="Waiting..." />
+    </FlexboxLayout>
+  </Page>
 </template>
 
 <script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
-    }
-  };
+import * as geolocation from "@nativescript/geolocation";
+import { CoreTypes } from "@nativescript/core";
+import now from "performance-now";
+
+
+
+export default {
+  data() {
+    return {
+      location: null,
+      msg:null
+    };
+  },
+  methods: {
+    getLocation: async function () {
+      await geolocation.enableLocationRequest();
+      let before = now();
+      let currentLocation = await geolocation.getCurrentLocation({
+        desiredAccuracy: CoreTypes.Accuracy.high,
+        maximumAge: 5000,
+        timeout: 20000,
+      });
+      let after = now();
+      let time = after-before;
+      this.msg = time + " miliseconds";
+      this.location = currentLocation;
+    },
+  },
+  mounted() {
+    this.getLocation();
+  },
+};
 </script>
 
 <style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
+@import "@nativescript/theme/scss/variables/blue";
 
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
+// Custom styles
+.fas {
+  @include colorize($color: accent);
+}
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+.info {
+  font-size: 20;
+  horizontal-align: center;
+  vertical-align: center;
+}
 </style>
