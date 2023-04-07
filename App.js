236d728124fe7288ff455875146a317a87557028
Matej Chlustik <matejchlustik221@gmail.com>
2023-04-07 14:05:59 +0200

    final

--- App.js
+++ App.js
@@ -1,14 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
+import now from 'performance-now';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      let beforeModule = now();
+      let before = performance.now();
+      let location = await Location.getCurrentPositionAsync({});
+      let after = performance.now();
+      let afterModule = now();
+      console.log(after - before, " miliseconds");
+      console.log(afterModule - beforeModule, "module miliseconds");
+      setLocation(location);
+    })();
+  }, []);
+
+  const getloc = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+    let beforeModule = now();
+    let before = performance.now();
+    let location = await Location.getCurrentPositionAsync({});
+    let after = performance.now();
+    let afterModule = now();
+    console.log(after - before, " miliseconds");
+    console.log(afterModule - beforeModule, "module miliseconds");
+    setLocation(location);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location ?
+        <View>
+          <Text style={styles.paragraph}>latitude: {location.coords.latitude}</Text>
+          <Text style={styles.paragraph}>longitude: {location.coords.longitude}</Text>
+          <Text style={styles.paragraph}>accuracy: {location.coords.accuracy}</Text>
+          <Button title="retry" onPress={getloc} />
+        </View>
+        :
+        errorMsg ?
+          <Text>Permission not granted</Text>
+          :
+          <Text>Waiting...</Text>
+      }
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
